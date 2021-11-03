28fa79563da7c2c6d412b1e0be7b335a6e8f8cbd
Rasmus Lind <rasmuslind@Rasmuss-MacBook-Air.local>
2021-11-03 10:07:04 +0100

    made get methods that grab login and register.ejs

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const app = express();
 //we use our dependency ejs 
 app.set("view-engine", "ejs")
 
+//get method that grabs index.ejs and our localhost default will go to this page
 app.get("/", (req, res) => {
-    res.render(index.ejs)
+    res.render("index.ejs")
+})
+
+//get method that grabs login.ejs 
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
+
+//get method that grabs register.ejs
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
 })
 
 app.listen(3000)
