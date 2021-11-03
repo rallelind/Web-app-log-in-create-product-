439f8b043437e940fbfeecd0c3ed0c42524e373a
Rasmus Lind <rasmuslind@Rasmuss-MacBook-Air.local>
2021-11-03 17:40:35 +0100

    made function in passport-config that sets up passport

--- passport-config.js
+++ passport-config.js
@@ -1,3 +1,27 @@
-const initalize = ((passport) => {
+const LocalStrategy = require("passport-local").Strategy //we use the passport-local library and
+const bcrypt = require("bcrypt");
 
+    const authenticatUser = ((email, password, done) => {
+        const user = getUserByEmail(email)
+        if(user == null) return done(null, false, { message: "No user with that email" })
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            }else{
+                return done(null, false, { message: "Password incorrect" })
+            }
+        }catch(e){
+            return done(e)
+        }
+    })
+
+
+
+const initalize = ((passport) => {
+    passport.use(new LocalStrategy({ usernameField: "email" }), authenticatUser)
+    passport.serializeUser((user, done) => { })
+    passport.deserializeUser((id, done) => { })
 })
+
+module.export = initalize
