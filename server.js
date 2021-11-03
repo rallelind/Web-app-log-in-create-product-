6b3d3cc99436f1267f6a9415780758421255fd79
Rasmus Lind <rasmuslind@Rasmuss-MacBook-Air.local>
2021-11-03 10:04:02 +0100

    made comments

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+//we use require to use express and save it as app
 const express = require("express");
 const app = express();
 
+//we use our dependency ejs 
 app.set("view-engine", "ejs")
 
 app.get("/", (req, res) => {
