219db73c61c253cc0ba5efdff85161d3e406729f
Rasmus Lind <rasmuslind@Rasmuss-MacBook-Air.local>
2021-11-27 10:57:54 +0100

    made unit test

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,32 +1,24 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const server = require("../api/routes/productRoutes");
+const { expect } = require("chai")
+const chai = require("chai")
+const chaiHttp = require("chai-http")
+const app = require("../app")
 
-chai.should()
+chai.use(chaiHttp)
 
-chai.use(chaiHttp);
-
-describe("rest API", () => {
-    describe("POST /", () => {
-        it("It should POST a new product", (done) => {
-            const product = {
-                description: "t-shirt",
-                price: "2$",
-                category: "clothes",
-                image: "img"
-            };
-            chai.request(server)
-                .post("/")
-                .send(product)
-                .end((res, err) => {
-                    res.should.have.status(200)
-                    res.body.should.be.a("object")
-                    res.body.should.have.property("description")
-                    res.body.should.have.property("price")
-                    res.body.should.have.property("category")
-                    res.body.should.have.property("img")
+describe("routes test", () => {
+    describe("GET /profile", () => {
+        it("it should render ejs file", (done) => {
+            chai
+            .request(app)
+            .get("/profile")
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res.status).to.equal(200)
+                expect(res.text).to.contain("name")
+                expect(res.body).to.be.an("object")
+                expect(res.body).to.not.equal("array")
                 done()
-                })
+            })
         })
     })
 })
